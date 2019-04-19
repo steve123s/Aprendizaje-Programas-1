0fcdb4a1d926a0db9a22b523808877d5049070de
Daniel Salinas <esteban@urbvan.com>
2019-04-19 15:14:52 -0500

    Add third program

--- ID3-3/ID3-3/ViewController.swift
+++ ID3-3/ID3-3/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  ID3-2
+//  ID3-3
 //
-//  Created by Daniel Salinas on 4/18/19.
+//  Created by Daniel Salinas on 4/19/19.
 //  Copyright © 2019 DanielSteven. All rights reserved.
 //
 
@@ -10,15 +10,35 @@ import UIKit
 import GameplayKit
 
 class ViewController: UIViewController {
-
+    
+    enum Winner: String {
+        case mexico = "México"
+        case alemania = "Alemania"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        executeTree()
+        
+        let treesInForest = 1000
+        var winners: [String: Int] = [Winner.mexico.rawValue: 0,
+                                      Winner.alemania.rawValue: 0]
+        
+        for _ in 0..<treesInForest {
+            if executeTree() == .mexico {
+                winners.updateValue(winners[Winner.mexico.rawValue]!+1, forKey: Winner.mexico.rawValue)
+            } else {
+                winners.updateValue(winners[Winner.alemania.rawValue]!+1, forKey: Winner.alemania.rawValue)
+            }
+        }
+        
+        let greatestProbability = winners.max { a, b in a.value < b.value }
+        
+        print("Predicción: \(String(describing: greatestProbability!.key)) con una efectividad del \(String(describing: greatestProbability!.value*100/treesInForest))%")
         
     }
-
-    private func executeTree() {
+    
+    private func executeTree() -> Winner {
         
         let attributes = [ "Hora?", "TipoJuego?", "CondicionesCancha?", "Local?" ]
         let examples = [
@@ -36,7 +56,7 @@ class ViewController: UIViewController {
             [ "Noche", "Mundial", "Pésima", "1" ],
             [ "Tarde", "Mundial", "Excelente", "1" ],
             [ "Tarde", "Olímpicos", "Mala", "1" ]
-            ]
+        ]
         let actions = [ // One for each entry in the examples array
             "Alemania",
             "Alemania",
@@ -57,7 +77,6 @@ class ViewController: UIViewController {
         let decisionTree = GKDecisionTree(examples: examples as NSArray as! [[NSObjectProtocol]],
                                           actions: actions as NSArray as! [NSObjectProtocol],
                                           attributes: attributes as NSArray as! [NSObjectProtocol])
-        print(decisionTree.description)
         
         let answers = [
             "Hora?": "Tarde",
@@ -68,8 +87,13 @@ class ViewController: UIViewController {
         
         let decision = decisionTree.findAction(forAnswers: answers as [AnyHashable : NSObjectProtocol])
         
-        print("Predicción: \(decision!)")
+        if String(describing: decision!) == Winner.mexico.rawValue {
+            return Winner.mexico
+        } else {
+            return Winner.alemania
+        }
+        
     }
-
+    
 }
 
